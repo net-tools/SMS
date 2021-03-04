feface98fe8eb63082e7270ac7028fce6581c842
Pierre <dev@nettools.ovh>
2021-03-04 17:27:51 +0100

    first commit

---
+++ src/SMSGateway.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Nettools\SMS;
+
+
+
+/**
+ * Classe pour envoi de SMS
+ */
+interface SMSGateway {
+
+	/**
+	 * Send SMS to several recipients
+	 *
+	 * @param string $msg Message to send
+	 * @param string $sender Sender id
+	 * @param string[] $to Array of recipients
+	 * @param string $nostop Nostop clause
+	 * @return int Returns the number of messages sent, usually the number of values of $to parameter (a multi-sms message count as 1 message)
+	 */
+	function send($msg, $sender, $to, $nostop = true);
+}
+
+
+?>
