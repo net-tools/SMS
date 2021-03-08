3df4aee19f09277ffbfa350f3236b9e4063449f5
Pierre <dev@nettools.ovh>
2021-03-08 13:32:03 +0100

    bulkSend

--- src/SMSGateway.php
+++ src/SMSGateway.php
@@ -20,6 +20,32 @@ interface SMSGateway {
 	 * @return int Returns the number of messages sent, usually the number of values of $to parameter (a multi-sms message count as 1 message)
 	 */
 	function send($msg, $sender, array $to, $nostop = true);
+	
+	
+		
+	/**
+	 * Send SMS to a lot of recipients (this is more optimized that calling `send` with a big array of recipients)
+	 *
+	 * @param string $msg 
+	 * @param string $sender
+	 * @param string[] $to Big array of recipients, numbers in international format +xxyyyyyyyyyyyyy (ex. +33612345678)
+	 * @param bool $transactional True if message sent is transactional ; otherwise it's promotional)
+	 * @return int Returns the number of SMS sent (a multi-sms message count as as many message)
+	 */
+	function bulkSend($msg, $sender, array $to, $transactional = true);
+	
+	
+	
+	/**
+	 * Send SMS to a lot of recipients by downloading a CSV file
+	 *
+	 * @param string $msg 
+	 * @param string $sender
+	 * @param string $url Url of CSV file with recipients, numbers in international format +xxyyyyyyyyyyyyy (ex. +33612345678), first row is column headers (1 column title 'Number')
+	 * @param bool $transactional True if message sent is transactional ; otherwise it's promotional)
+	 * @return int Returns the number of SMS sent (a multi-sms message count as as many message)
+	 */
+	function bulkSendFromHttp($msg, $sender, $url, $transactional = true);	
 }
 
 
